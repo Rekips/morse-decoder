9a62ef5af7ec583c6e6f3eefc945f51d399e129a
Rekips <50841648+Rekips@users.noreply.github.com>
2023-08-03 09:52:50 +0600

    feat: Update index.js

--- src/index.js
+++ src/index.js
@@ -39,7 +39,32 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+	let word = 0, wc=0,  a = [], res='';
+	a[0]='';
+	for (let i=0; i<expr.length; i+=2) {
+		if (expr.slice(i, i+2)==='10') {
+			a[word]+='.';
+		} else if (expr.slice(i, i+2)==='11') {
+			a[word]+='-';
+		} else if (expr.slice(i, i+2)==='**') {
+			a[word]+='*';
+		}
+		wc++;
+		if (wc === 5) {
+			if (a[word] in MORSE_TABLE) {
+				res+=MORSE_TABLE[a[word]];
+			} else {
+				res+=' ';
+			}
+			word++;
+			wc=0;
+			a[word]='';
+		}
+	}
+	return res;
 }
+
+console.log(decode("00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010"));
 
 module.exports = {
     decode
